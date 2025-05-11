d39497b96e69751d8ac35793b9e7986bf5550fb5
Lukasz Greszczuk <lukasz.greszczuk@pl.bosch.com>
2025-05-11 23:41:05 +0200

    modify the app

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,7 +86,7 @@ resource "azurerm_linux_web_app" "main" {
   service_plan_id     = azurerm_service_plan.main.id
 
   site_config {
-    always_on = true
+    always_on = false
   }
 
   app_settings = {
